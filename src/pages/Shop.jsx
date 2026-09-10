57980cbc599c1bc7f9dce3c05b88ecfbe6a63f3e
masumprajapati5 <pm04092005@gmail.com>
2026-09-10 10:32:27 -0700

    skymart some comment add

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -53,6 +53,7 @@ const Shop = () => {
     recompute(searched, cat, sortType)
   }
 
+  // shop page ui
   return (
     <main className="min-h-screen bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
