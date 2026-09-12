8c9f823677aebe9d612adc8640ea4f223ff2bda6
masumprajapati5 <pm04092005@gmail.com>
2026-09-12 10:39:30 -0700

    skymart some comment add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ const Home = () => {
   const { PRODUCTS } = useContext(MyStore)
   const sortratings = sortProducts(PRODUCTS, "price-desc")
 
+
+  // home page ui
   return (
     <main className="min-h-screen bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
